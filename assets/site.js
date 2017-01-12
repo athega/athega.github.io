85f463be8906a720793300af05bfffb0bd367583
Johan Beronius <johan.beronius@athega.se>
2017-01-12 18:53:37 +0100

    Bundle scripts.

---
+++ assets/site.js
@@ -0,0 +1,11 @@
+---
+---
+{% comment %} Concatenation and basic whitespace minification of JavaScript files {% endcomment %}{% capture js %}
+
+{% include js/jquery-3.1.1.min.js %}
+{% include js/utils.js %}
+{% include js/script.js %}
+{% include js/owl.carousel.min.js %}
+
+{% endcapture %}{% assign lines = js | split: "
+" %}{% for line in lines %}{% assign str = line | strip %}{% assign comment = str | truncate: 2, "" %}{% unless comment == '//' %}{{ str }}{% endunless %}{% endfor %}
