47da3c56914ba41c1230ada5aea2349638d4144b
Johan Beronius <johan.beronius@athega.se>
2017-01-16 17:56:49 +0100

    Cleaned markup and styles for header and footer.

--- _includes/header.js
+++ _includes/header.js
@@ -1,12 +1,13 @@
 $(function() {
     'use strict';
 
-    var mobileThreshold = 720,
+    var $header = $('body > header'),
+        mobileThreshold = 720,
         squizedHeaderThresh = 100;
 
     // Resize header on scroll
     function resizeHeader() {
-        $('#site-header').toggleClass('small', window.innerWidth > mobileThreshold && window.pageYOffset > squizedHeaderThresh);
+        $header.toggleClass('small', window.innerWidth > mobileThreshold && window.pageYOffset > squizedHeaderThresh);
     }
 
     $(window).on('scroll', resizeHeader);
