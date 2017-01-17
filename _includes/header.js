5adbc7a6a893db3964cf79bad35e7594c28125a2
Johan Beronius <johan.beronius@athega.se>
2017-01-17 15:01:44 +0100

    Header should minimize height, especially on mobile.

--- _includes/header.js
+++ _includes/header.js
@@ -1,16 +1,12 @@
 $(function() {
     'use strict';
 
-    var $header = $('body > header'),
-        mobileThreshold = 720,
-        squizedHeaderThresh = 100;
+    var $header = $('body > header');
 
-    // Resize header on scroll
     function resizeHeader() {
-        $header.toggleClass('small', window.innerWidth > mobileThreshold && window.pageYOffset > squizedHeaderThresh);
+        $header.toggleClass('small', document.documentElement.scrollTop > 100);
     }
 
     $(window).on('scroll', resizeHeader);
     resizeHeader();
-
 });
