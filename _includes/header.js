ec4fd45ee0f21b27bffcf1a0db987c9f4448b7a4
Johan Beronius <johan.beronius@athega.se>
2017-01-18 14:49:50 +0100

    Fixed sizing of main menu button and top level items.

--- _includes/header.js
+++ _includes/header.js
@@ -4,7 +4,7 @@ $(function() {
     var $header = $('body > header');
 
     function resizeHeader() {
-        $header.toggleClass('small', document.documentElement.scrollTop > 100);
+        $header.css('height', (4 - 2 * (Math.max(100, Math.min(200, document.documentElement.scrollTop)) - 100) / 100) + 'em');
     }
 
     $(window).on('scroll', resizeHeader);
