910a0770f9ee57ed175920c368767e08db642192
Johan Beronius <johan.beronius@athega.se>
2017-03-16 19:06:14 +0100

    Added scratch-to-reveal effect on tagline.

--- assets/site.js
+++ assets/site.js
@@ -7,6 +7,7 @@
 {% include header.js %}
 {% include header/main-menu.js %}
 {% include home/intro.js %}
+{% include home/intro-code.js %}
 {% include home/blog.js %}
 
 {% endcapture %}{% assign lines = js | split: "
