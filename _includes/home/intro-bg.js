4d0cbbaea23ae51ebe698ed3461de913f7924327
Christian Lizell <christian.lizell@aftonbladet.se>
2018-04-20 16:26:26 +0200

    feat: Simplify design
    * Removed animations and new concept image
    * Narrower desktop
    * Larger font
    * Three blog posts
    * New roles design

--- _includes/home/intro-bg.js
+++ _includes/home/intro-bg.js
@@ -27,7 +27,7 @@ $(function() {
     createShaders();
     createVertices();
 
-    image.src = "/assets/img/riddarfjarden.jpg";
+    image.src = "https://ak6.picdn.net/shutterstock/videos/17381566/thumb/1.jpg";
     image.onload = function() {
         createTexture(image);
         draw();
--- assets/site.js
+++ assets/site.js
@@ -6,8 +6,6 @@
 {% include lib/jquery-3.2.1.min.js %}
 {% include header.js %}
 {% include header/main-menu.js %}
-{% include home/intro.js %}
-{% include home/intro-bg.js %}
 {% include home/blog.js %}
 
 {% endcapture %}{% assign lines = js | split: "
