e335df43412c71b46848d55bb534d9093cf959f6
Christian Lizell <christian.lizell@athega.se>
2019-02-08 16:50:19 +0100

    feat: Move all legacy image uploads into this repo

---
+++ assets/legacy/uploads/2017/04/convert.sh
@@ -0,0 +1,2 @@
+convert gopher-brown-beard.crushed.png -trim +repage gopher-brown-beard.trimmed.png
+convert gopher-brown-beard.trimmed.png -background transparent -gravity south -extent 2200x1152 gopher-brown-beard.png
